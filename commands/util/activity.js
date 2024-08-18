9d5bd077d2a3e2341f7f6f26f090312a6b3df2f6
MsVoxxie <odysseylove222@gmail.com>
2024-08-17 22:51:40 -0400

    Added activity command

---
+++ commands/util/activity.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const { dateToRelative } = require('../../functions/helpers/dateFormatters');
+const watchedUsers = require('../../models/watchedUsers');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('activity')
+		.setDescription('Check the activity of a user')
+		.addUserOption((option) => option.setName('user').setDescription('The user to check').setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+	options: {
+		devOnly: false,
+		disabled: false,
+	},
+	async execute(client, interaction) {
+		// Declarations
+		const checkedUser = interaction.options.getUser('user');
+		const checkedMember = interaction.guild.members.cache.get(checkedUser.id);
+
+		// Fetch oldest members from database
+		const inactiveUser = await watchedUsers.find({ guildId: interaction.guild.id, userId: checkedUser.id });
+		if (!inactiveUser.length) return interaction.reply('User has not been tracked yet.');
+
+		// Create the embed
+		const embed = new EmbedBuilder()
+			.setTitle('User Activity')
+			.setDescription(`${checkedMember ? checkedMember : `<@${checkedUser.id}>`}'s Latest Activity`)
+			.setColor(client.color)
+			.setTimestamp()
+			.addFields({ name: 'Last Interaction', value: dateToRelative(inactiveUser[0].lastInteraction) }, { name: 'Last Action', value: inactiveUser[0].lastAction });
+
+		// Send the embed
+		return interaction.reply({ embeds: [embed] });
+	},
+};
